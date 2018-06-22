292157fa72c7481cd8fa8b21f3e16d2c019b0da5
Albreis Network <albreisdev@gmail.com>
2018-06-22 11:47:07 -0300

    Adicionado variavel de configuracao do grid

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+var gridSettings = {
+  containerId: 'grid-conteiner', 
+  cellClass: 'cell',
+  cols: 15,  // Quantidade máxima de colunas
+  cw: 300, // Largura mímina da coluna e células
+  margin: 10, // Margin entre as células
+  padding: 10 // Padding das células
+}
 window.onload = function() {
     if (document.getElementById('grid-conteiner'))
-        gengrid('grid-conteiner', 'cell');
+        gengrid(gridSettings);
 };
 var tOut = false;
 var milSec = 500;
@@ -9,6 +17,6 @@ window.onresize = function() {
         // Cria um "delay" para o evento onresize para não ocorrer overflow na fila de animações
         if (tOut !== false)
             clearTimeout(tOut);
-        tOut = setTimeout(gengrid, milSec);
+        tOut = setTimeout(gengrid(gridSettings), milSec);
     }
 };
